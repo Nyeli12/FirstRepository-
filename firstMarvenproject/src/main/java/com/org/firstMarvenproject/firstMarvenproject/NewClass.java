78a5ab3e7a7e633f3411480b5a2077041e1ab4e1
NyBaloyi <NyBaloyi@dojcd00030781NO.dojcd.gov.za>
2021-04-13 16:25:02 +0200

    FirstCommit to Master branch

---
+++ firstMarvenproject/src/main/java/com/org/firstMarvenproject/firstMarvenproject/NewClass.java
@@ -0,0 +1,13 @@
+package com.org.firstMarvenproject.firstMarvenproject;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi team");
+		
+
+	}
+
+}
